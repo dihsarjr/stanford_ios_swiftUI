e2a9199472184fed748221edc2c8e68133d469bd
rashid <mohammed1rashid786@gmail.com>
2021-06-13 21:06:12 +0530

    start

--- Memoraize/ContentView.swift
+++ Memoraize/ContentView.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State var list:[String] = ["🙈","🐵","🐽","🦑","🐡","🐢","🐍","🐛","🐳","🦭","🐊","👛","🐒","💍","🪖","🦚","🌏","❄️","🔥","🥝"]
+    @State var emogiCount:Int = 5
+    
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                .stroke()
-                .stroke(lineWidth: 3)
-            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-        }.padding().foregroundColor(.red)
+        VStack{
+        ScrollView{
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 80, maximum: 100))],content: {
+                ForEach(list[0...emogiCount],id:\.self){emoji in CardView(emogi: emoji).aspectRatio(1, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)}
+            }).padding()
+        }.foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Spacer()
+            HStack{
+                Button(action: {
+                    if emogiCount<list.count-1 {
+                        emogiCount += 1
+                    }
+                }, label: {
+                    Image(systemName: "plus.app")   .resizable()
+                        .frame(width: 40.0, height:40.0)
+                })
+                Spacer()
+                Button(action: {
+                    list.shuffle()
+                }, label: {
+                    Text("SHUFFLE").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                })
+               
+                Spacer()
+                Button(action: {
+                    if emogiCount>0 {
+                        emogiCount -= 1
+                    }
+                }, label: {
+                    Image(systemName: "minus.rectangle")
+                        .resizable()
+                        .frame(width: /*@START_MENU_TOKEN@*/40.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/40.0/*@END_MENU_TOKEN@*/)
+                })
+                
+            }.padding()
+        }
+ 
     }
 }
 
@@ -23,3 +57,27 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CardView: View {
+   @State var isFaceUp:Bool = false
+    var emogi:String
+    var body: some View {
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke()
+                    .stroke(lineWidth: 3)
+                Text(emogi).font(.largeTitle).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            }else{
+                RoundedRectangle(cornerRadius: 8)
+                    .fill()
+                    
+            }
+     
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
